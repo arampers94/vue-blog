b5743ecc5ad99f9ef0ff441a0f5dc8100f440297
Amar Rampersaud <arampers94@gmail.com>
2019-08-27 12:34:56 -0400

    implemented custom search filter and mixins

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -1,17 +1,15 @@
 <template>
   <div v-theme:column="narrow" id="show-blogs">
-    <h1>All Blog Articles</h1>
+    <h1>List Blog Titles</h1>
     <input type="text" v-model="search" placeholder="Search blogs" />
     <div v-for="(blog, index) in filteredBlogs" v-bind:key="index" class="single-blog">
       <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
-      <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
 
 <script>
 import searchMixin from "../mixins/searchMixin";
-
 export default {
   data() {
     return {
