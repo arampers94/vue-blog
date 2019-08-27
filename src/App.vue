66b6272ddeebc1a9132434825b0fda349550438e
Amar Rampersaud <arampers94@gmail.com>
2019-08-26 20:14:00 -0400

    implemented input data binding

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <add-blog></add-blog>
+  </div>
 </template>
 
 <script>
+import addBlog from "./components/addBlog";
+
 export default {
+  components: {
+    "add-blog": addBlog
+  },
   data() {
     return {};
-  }
+  },
+  methods: {}
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
 </style>
